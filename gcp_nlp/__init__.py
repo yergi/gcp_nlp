863ea5a66fce94c56e28a37aaa2c082a3bac8386
James Colvin <jecolvin@outlook.com>
2018-10-26 16:46:04 -0400

    Update gcp_nlp/__init__.py
    Update to include new functions

--- gcp_nlp/__init__.py
+++ gcp_nlp/__init__.py
@@ -1,2 +1,3 @@
 from .functions import create_dataset, create_model, import_data, \
-    upload_data, get_model, get_operation_status
+    upload_data, get_model, get_operation_status, predict, \
+    get_model_evaluation, list_model_evaluations
